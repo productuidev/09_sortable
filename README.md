e654249dfbada26f5b9abc4e689fde5ae54d53ff
productuidev <productuidev@gmail.com>
2023-02-03 07:18:42 +0900

    del

--- README.md
+++
@@ -1,6 +0,0 @@
-# Sortable
-
-drag & drop list
-
-> npm install
-
--- src/TestItem/testData.js
+++
@@ -1,8 +0,0 @@
-// dummy data
-export const data = [
-  {content: 'REACT'},
-  {content: 'VUE'},
-  {content: 'ANGULAR'},
-  {content: 'SVELTE'},
-  {content: 'NEXT.js'},
-];
